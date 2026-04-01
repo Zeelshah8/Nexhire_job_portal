21be5fcebd355a8d03e3a6076a0770ec37bf9097
Zeel Shah <123805010+Zeelshah8@users.noreply.github.com>
2026-04-02 00:55:38 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Hero from '../components/Hero.jsx';
+import TopNiches from '../components/TopNiches.jsx';
+import HowITWorks from '../components/HowITWorks.jsx';
+ 
+const Home=()=>{
+    return(
+        <>
+
+            <Hero/>
+            <TopNiches/>
+            <HowITWorks/>
+
+        </>
+    )
+}
+
+export default Home
