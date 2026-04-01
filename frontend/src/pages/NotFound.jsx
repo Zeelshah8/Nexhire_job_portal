0966b4e5662cf2bb3547723ce494c040e91a127e
Zeel Shah <123805010+Zeelshah8@users.noreply.github.com>
2026-04-02 00:57:48 +0530

    Create NotFound.jsx

---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+ 
+const NotFound=()=>{
+    return(
+        <section className="notfound">
+            <div className="content">
+                <h1>404 Not Found</h1>
+                <p>visited page not found.</p>
+                <Link to={"/"} className="btn">Back to home</Link>
+            </div>
+        </section>     
+    )
+}
+export default NotFound
